09b2c6e5d9abf2dd62dce2f4acea7589343c0411
Piter Merlo Bruner <68957875+pitermb@users.noreply.github.com>
2022-06-20 15:55:37 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { splitVendorChunkPlugin } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), splitVendorChunkPlugin(), tsconfigPaths()],
+  build: { manifest: true },
+  base: process.env.NODE_ENV === "production" ? "/static/" : "/",
+  root: ".",
+});
